e3de673ae730ce9ae1320997e604c3c62131bcb6
Denis Lisyutenko <denislisyutenko@gmail.com>
2022-06-07 20:21:24 +0300

    responsive block header and skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,3 +7,11 @@ $(document).ready(function () {
   });
 
 });
+
+
+$(document).ready(function(){
+	$('.header-navbar__btn').on('click', function() {
+		$(this).toggleClass('header-navbar__btn--active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
+	});
+});
